31ed14be438b20481bf9968c7a7fa151211819bb
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-15 15:33:39 +0000

    learning about wordpress built in functions

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,12 +1,10 @@
-<?php 
-    function greet($name, $color){
-        echo "<p>Hi, my name is $name and my favourite color is $color.</p>";
-    };
+<?php
 
-    greet("John", "Blue");
-    greet("Jane", "Green");
+while (have_posts()) {
+    the_post(); ?>
+    <h2> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+<?php }
+
 ?>
-
-<h1><?php bloginfo('name');?></h1>
-
-<p><?php bloginfo('description'); ?></p>
