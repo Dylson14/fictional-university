2f7389ac3f0b3cc7fb862de93483d1555733c90c
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-18 15:33:06 +0000

    created the archives.php file and added the content for a single archive

--- app/public/wp-content/themes/fictional-university-theme/single.php
+++ app/public/wp-content/themes/fictional-university-theme/single.php
@@ -18,12 +18,32 @@ while (have_posts()) {
        and then sets the current post by moving, each time, to the next
        post in queue */
     the_post(); ?>
-    <!-- the_permalink() displays the permalink (the URL) for the current post, within the loop. It's the specific link to a specific page or post in your wordpress site -->
-    <br>
-    <br>
-    <h2><?php the_title(); ?></h2>
-    <p> <?php the_content(); ?> </p>
-<?php };
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('./images/ocean.jpg'); ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"> <?php the_title(); ?> </h1>
+            <div class="page-banner__intro">
+                <p>DON'T FORGET TO REPLACE ME LATER!</p>
+            </div>
+        </div>
+    </div>
+    <div class="container container--narrow page-section">
+
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <!-- get_the_title() explan: allows you to pass in an ID number in the parentheses and it will give you the title for that item instead of the currnet item you're on -->
+                    <a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a>
+                    <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('M.j.Y'); ?> in <?php echo get_the_category_list(', ') ?></span>
+                </p>
+            </div>
+            <div class="generic-content">
+                <?php the_content(); ?>
+            </div>
+
+        </div>
+
+    <?php };
 /* get_footer() loads footer template, same thing as the get_header() */
 get_footer();
-?>
+    ?>
