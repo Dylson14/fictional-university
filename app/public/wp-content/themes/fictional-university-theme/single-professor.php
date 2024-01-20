49a210e9d06743c028bfe82b2a5d139872327050
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-20 12:10:37 +0000

    created the needed php code to correctly iterate through the professor post data and output the needed information

---
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -0,0 +1,67 @@
+<!-- File Description: 
+This is a single page template specific to the event post type, by naming
+our files like single-event the keyword of our custom post type we can
+create files like this and customise their looks -->
+
+<?php
+/* get_header(), Docs: Loads the header template, includes the header template
+for a theme or if a name is specified, then a specialized header will be included 
+GPT: used to retrieve and include the header template file in a theme, such template is usually found in the header.php file */
+get_header();
+/* have_posts(), determines whether current Wordpress query has posts to loop over
+    GPT: it's a function used within the loop to check if there are any posts remaining 
+    in the current query */
+while (have_posts()) {
+    /* the function the_post() checks whether the loop has started 
+       and then sets the current post by moving, each time, to the next
+       post in queue */
+    the_post(); ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('./images/ocean.jpg'); ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"> <?php the_title(); ?> </h1>
+            <div class="page-banner__intro">
+                <p>DON'T FORGET TO REPLACE ME LATER!</p>
+            </div>
+        </div>
+    </div>
+    <div class="container container--narrow page-section">
+
+        <div class="container container--narrow page-section">
+
+            <div class="generic-content">
+                <div class="row group">
+
+                    <div class="one-third">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+
+                    <div class="two-thirds">
+                        <?php the_content(); ?>
+                    </div>
+
+                </div>
+            </div>
+
+            <?php
+            $relatedPrograms = get_field('related_programs');
+
+            if ($relatedPrograms) {
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+                echo '<ul class="link-list min-list">';
+                foreach ($relatedPrograms as $program) {
+            ?>
+                    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+            <?php }
+                echo '</ul>';
+            }
+
+            ?>
+
+        </div>
+
+    <?php };
+/* get_footer() loads footer template, same thing as the get_header() */
+get_footer();
+    ?>
--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -39,6 +39,37 @@ while (have_posts()) {
                 <?php the_content(); ?>
             </div>
             <?php
+            $relatedProfessors = new WP_Query(array(
+                'posts_per_page' => -1,
+                'post_type' => 'professor',
+                'orderby' => 'title',
+                'order' => 'ASC',
+                'meta_query' => array(
+                    array(
+                        'key' => 'related_programs',
+                        'compare' => 'LIKE',
+                        'value' => '"' . get_the_ID() . '"'
+                    )
+                )
+            ));
+
+            if ($relatedProfessors->have_posts()) {
+                echo '<hr class="section-break">';
+                echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
+
+                while ($relatedProfessors->have_posts()) {
+                    $relatedProfessors->the_post(); ?>
+                    <li class="professor-card__list-item">
+                        <a class="professor-card" href="<?php the_permalink(); ?>">
+                            <img class="professor-card__image" src="" alt="">
+                            <span class="professor-card__name"></span>
+                        </a>
+                    </li>
+                <?php }
+            }
+
+            wp_reset_postdata();
+
             $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
                 'posts_per_page' => 2,
@@ -63,40 +94,40 @@ while (have_posts()) {
 
             if ($homepageEvents->have_posts()) {
                 echo '<hr class="section-break">';
-            echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+                echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Professors</h2>';
 
-            while ($homepageEvents->have_posts()) {
-                $homepageEvents->the_post(); ?>
-                <div class="event-summary">
-                    <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month">
-                            <?php
-                            /* get_field() is a custom func from the ACF plugin, the error appears because it does not have the file access to the ACF plugin, 
+                while ($homepageEvents->have_posts()) {
+                    $homepageEvents->the_post(); ?>
+                    <div class="event-summary">
+                        <a class="event-summary__date t-center" href="#">
+                            <span class="event-summary__month">
+                                <?php
+                                /* get_field() is a custom func from the ACF plugin, the error appears because it does not have the file access to the ACF plugin, 
                         but it works as intended */
-                            $eventDate = new DateTime(get_field('event_date'));
-                            echo $eventDate->format('M');
-                            ?>
-                        </span>
-                        <span class="event-summary__day">
-                            <?php
-                            $eventDate = new DateTime(get_field('event_date'));
-                            echo $eventDate->format('d');
-                            ?>
-                        </span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p> <?php if (has_excerpt()) {
-                                echo get_the_excerpt();
-                            } else {
-                                echo wp_trim_words(get_the_content(), 18);
-                            }
-                            ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                                $eventDate = new DateTime(get_field('event_date'));
+                                echo $eventDate->format('M');
+                                ?>
+                            </span>
+                            <span class="event-summary__day">
+                                <?php
+                                $eventDate = new DateTime(get_field('event_date'));
+                                echo $eventDate->format('d');
+                                ?>
+                            </span>
+                        </a>
+                        <div class="event-summary__content">
+                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                            <p> <?php if (has_excerpt()) {
+                                    echo get_the_excerpt();
+                                } else {
+                                    echo wp_trim_words(get_the_content(), 18);
+                                }
+                                ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                        </div>
                     </div>
-                </div>
             <?php }
             }
-            
+
             ?>
         </div>
 
